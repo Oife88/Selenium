c2e05b679078bca94e328d175af876b3692f63d8
Evgenia <z9262792@yandex.ru>
2023-02-10 18:52:58 +0300

    Tests2 commit

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -13,18 +13,63 @@ class CallbackTest {
 
 
     @Test
-    void shouldTestSomething()  {
+    void shouldTestSomething() {
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Иванов Василий");
         $("[data-test-id=phone] input").setValue("+79009990909");
         $("[data-test-id=agreement]").click();
         $(By.className("button__text")).click();
         $(By.tagName("p")).shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+        @Test
+        void shouldTestSomething2 () {
+            open("http://localhost:9999/");
+            $("[data-test-id=name] input").setValue("Ivanov Василий");
+            $("[data-test-id=phone] input").setValue("+79009990909");
+            $("[data-test-id=agreement]").click();
+            $(By.className("button__text")).click();
+            $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
 
+        }
+        @Test
+        void shouldTestSomethingTel () {
+            open("http://localhost:9999/");
+            $("[data-test-id=name] input").setValue("Иванов Василий");
+            $("[data-test-id=phone] input").setValue("8909965606");
+            $("[data-test-id=agreement]").click();
+            $(By.className("button__text")).click();
+            $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+
+        }
+
+        @Test
+        void shouldTestSomethingName () {
+            open("http://localhost:9999/");
+            $("[data-test-id=phone] input").setValue("+79099656060");
+            $("[data-test-id=agreement]").click();
+            $(By.className("button__text")).click();
+            $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+
+        }
+        @Test
+
+        void shouldTestSomethingPhone () {
+            open("http://localhost:9999/");
+            $("[data-test-id=name] input").setValue("Иванов Василий");
+            //$("[data-test-id=phone] input").setValue("");
+            $("[data-test-id=agreement]").click();
+            $(By.className("button__text")).click();
+            $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+
+        }
 
 
     }
 
-}
+
+
 
